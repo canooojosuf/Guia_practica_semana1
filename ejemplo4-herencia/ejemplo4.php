65ec48ccfca53149adb3d559b1b28b1c1bcb8391
Jose Ulises Ceron Ramos <jose.ceron67257@uped.edu.sv>
2026-07-24 19:34:05 -0600

    Agregar ejemplo 4

---
+++ ejemplo4-herencia/ejemplo4.php
@@ -0,0 +1,51 @@
+<?php 
+abstract class Vehiculo 
+{ 
+    protected string $marca; 
+ public function __construct(string $marca) 
+    { 
+        $this->marca = $marca; 
+    } 
+  
+    public function arrancar(): string 
+    { 
+        return "{$this->marca}: el motor enciende."; 
+    } 
+} 
+  
+class Carro extends Vehiculo 
+{ 
+    public function abrirCajuela(): string 
+    { 
+        return "{$this->marca}: cajuela abierta."; 
+    } 
+} 
+  
+class Moto extends Vehiculo 
+{ 
+    public function hacerCaballito(): string 
+    { 
+        return "{$this->marca}: ¡wheelie!"; 
+    } 
+} 
+
+class Bicicleta extends Vehiculo 
+{ 
+    public function tocarTimbre(): string 
+    { 
+        return "{$this->marca}: ¡ding ding!"; 
+    } 
+}
+  
+$carro = new Carro("Toyota Corolla"); 
+$moto = new Moto("Honda CB190R"); 
+$bicicleta = new Bicicleta("BMX 3"); 
+  
+echo $carro->arrancar() . PHP_EOL; 
+echo $carro->abrirCajuela() . PHP_EOL; 
+echo $moto->arrancar() . PHP_EOL; 
+echo $moto->hacerCaballito() . PHP_EOL; 
+echo $bicicleta->arrancar() . PHP_EOL; 
+echo $bicicleta->tocarTimbre() . PHP_EOL; 
+
+
